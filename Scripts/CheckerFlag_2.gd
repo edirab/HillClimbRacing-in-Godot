d42f5068cedb9c53d4f1ef500438b97f19359285
Eugene <edirab@yandex.ru>
2022-08-30 22:07:12 +0200

    - Added LevelCompletedTimer, updated logic in CheckerFlag script

--- Scripts/CheckerFlag_2.gd
+++ Scripts/CheckerFlag_2.gd
@@ -1,20 +1,27 @@
 extends Area2D
 
+var level_completed = false
 
 func _ready():
 	pass
 
 
-
 func _on_CheckerFlag_body_entered(body):
-	if body.is_in_group("player"):
+	
+	if body.is_in_group("player") && !level_completed:
+		level_completed = true
 		print("Level completed!")
 		get_tree().get_current_scene().level_completed()
-		var level_name = get_tree().get_current_scene().name
-		print(level_name)
 		
-		if (level_name == "Level1"):
-			get_tree().change_scene("res://Scenes/Levels/Level2.tscn")
-		elif (level_name == "Level2"):
-			print("Level3 is not done yet")
-		
+		if $LevelFinishedTimer.is_stopped():
+			$LevelFinishedTimer.start()	
+
+
+func _on_LevelFinishedTimer_timeout():
+	
+	var level_name = get_tree().get_current_scene().name
+	print(level_name)
+	if (level_name == "Level1"):
+		get_tree().change_scene("res://Scenes/Levels/Level2.tscn")
+	elif (level_name == "Level2"):
+		print("Level3 is not done yet")
