2a0f48f8bef725f4f8d49448c10a93f81434a9ab
Eugene <edirab@yandex.ru>
2022-09-02 19:16:46 +0200

    - Designed Main menu and Options scene. - Created custom slider for volume

---
+++ Scripts/Options.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+func _ready():
+	pass
+
+
+func _on_VolumeSlider_value_changed(value):
+	pass # Replace with function body.
+
+
+
+func _on_BackButton_pressed():
+	queue_free()
